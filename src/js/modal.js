ddf518ab5dec17c2094a3691a0153ff29619047f
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-12-21 16:43:58 +0200

    add fix modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,15 @@
 const cross = document.querySelector(".popup__cross");
-const items = document.querySelectorAll(".popup-list__item");
 const links = document.querySelectorAll(".fonts-list__link");
+const popup = document.querySelector("#popup");
+
+cross.addEventListener("click", () => {
+  popup.classList.remove("popup--visible");
+  document.body.classList.remove("popup-visible");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    popup.classList.add("popup--visible");
+    document.body.classList.add("popup-visible");
+  });
+});
