e60ea474dd47e985b52e68dc49106c60cf0f7132
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-12-22 09:58:54 +0200

    fix modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,9 @@
 const cross = document.querySelector(".popup__cross");
 const links = document.querySelectorAll(".fonts-list__link");
 const popup = document.querySelector("#popup");
+const numberModal = document.querySelector(".popup-list__number");
+const textModal = document.querySelector(".popup-list__text");
+const projectModal = document.querySelector(".popup-list__img");
 
 cross.addEventListener("click", () => {
   popup.classList.remove("popup--visible");
@@ -11,5 +14,15 @@ links.forEach((link) => {
   link.addEventListener("click", (e) => {
     popup.classList.add("popup--visible");
     document.body.classList.add("popup-visible");
+
+    console.log(
+      link.dataset.number,
+      link.dataset.text,
+      link.dataset.preview,
+      link.dataset.project
+    );
+    // link.dataset.text;
+    // link.dataset.preview;
+    // link.dataset.project;
   });
 });
